31bdb96b5b4aa8ff4b1f96363614121ff4864a16
ericpo1sh <ericpo1sh@gmail.com>
2024-03-29 10:27:25 -0500

    added responsivevness to vide carousel, need to fix small breakpoint

--- scripts.js
+++ scripts.js
@@ -96,7 +96,34 @@ $(document).ready(function() {
           $('.carousel-slick').slick({
             infinite: true,
             slidesToShow: 4,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  infinite: true,
+                }
+              },
+              {
+                breakpoint: 860,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 580,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+              // You can unslick at a given breakpoint now by adding:
+              // settings: "unslick"
+              // instead of a settings object
+            ]
           })
         },
         error: function() {
